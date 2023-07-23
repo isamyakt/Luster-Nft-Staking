55d0ba21b1d46d6e74ed5c7d4b8ed2a3de2b5640
isamyakt <isamyakt17@gmail.com>
2023-07-23 19:28:36 +0530

    NFT Staking, NFTs, Token Contract Addresses ☣️

---
+++ consts/contractAddress.ts
@@ -0,0 +1,3 @@
+export const nftDropContractAddress = "0xbA128318E68e59FCEf744abf0Db947a8915052A7";
+export const tokenContractAddress = "0xfE852F888F627CC5410D857Dd975c97a439BAaA6";
+export const stakingContractAddress = "0x1dE7351a4687b3ddb16A7A711c3948fDAb040F6B"
